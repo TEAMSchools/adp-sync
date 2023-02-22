1c8f94f312c21e490506dce7fa08bef1aed48519
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-02-22 12:45:04 -0600

    Initial commit

---
+++ .devcontainer/scripts/postCreate.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# update apt packages
+sudo apt-get -qq -y update --no-install-recommends &&
+	sudo apt-get -qq -y install --no-install-recommends bash-completion &&
+	sudo apt-get -qq -y upgrade --no-install-recommends &&
+	sudo apt-get -qq autoremove -y &&
+	sudo apt-get -qq clean -y
+
+# update pip
+python -m pip install --no-cache-dir --upgrade pip
+
+# install Trunk
+curl https://get.trunk.io -fsSL | bash -s -- -y
+trunk install --ci
+
+# install pdm dependencies
+pdm install --no-self
+
+# commit new files
+git add .
+git commit -m "Initial PDM commit"
+git push
