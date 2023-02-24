35db339dd0e734885b588e193a321b1c23586a80
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-02-24 21:13:36 +0000

    fix

--- .pdm/scripts/generate-csr.sh
+++ .pdm/scripts/generate-csr.sh
@@ -3,5 +3,4 @@
 openssl genrsa -out env/kipptaf_auth.key 2048
 openssl req -new \
 	-key env/kipptaf_auth.key \
-	-out env/kipptaf_auth.csr \
-	-subj "/C=US/ST=NJ/L=Newark/O=KIPP TAF/OU=Data/CN=KIPP TAF ADP API Access"
+	-out env/kipptaf_auth.csr
